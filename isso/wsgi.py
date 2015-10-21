b6b0fb79ecb5d68eabaf431d04dbd22f3637f670
tyc85 <steven.tychen@gmail.com>
2015-10-20 22:42:54 -0700

    solve status not string issue on uwsgi

--- isso/wsgi.py
+++ isso/wsgi.py
@@ -141,7 +141,7 @@ class CORSMiddleware(object):
                 headers.add("Access-Control-Allow-Headers", ", ".join(self.allowed))
             if self.exposed:
                 headers.add("Access-Control-Expose-Headers", ", ".join(self.exposed))
-            return start_response(status, headers.to_list(), exc_info)
+            return start_response(str(status), headers.to_list(), exc_info)
 
         if environ.get("REQUEST_METHOD") == "OPTIONS":
             add_cors_headers(b"200 Ok", [("Content-Type", "text/plain")])
